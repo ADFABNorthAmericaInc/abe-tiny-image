a38dd8f8f0a1658a18a89dbaa55bb9544d6455c9
Simon Conan <simon.conan@adfab.com>
2018-11-27 17:02:54 -0500

    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# abe-tiny-image
+# Abe-tiny-image
+
+A little bash script, which reduce the size of all images in a folder
+
+## Require
+You need to have curl installed to be able to use this script. If you don't
+have it just run this command :
+```
+	sudo apt-get install curl
+```
+
+## Installation
+Just add this function to your .bashrc file. You can just launch this :
+```
+	cat tinypng >> ~/.bashrc
+```
+You need to create a .env file at the root of the project. When you are registerid on
+tinypng.com you just have to fill in your apikey in the .env file.
+
+## Utilisation
+Go to a folder full of images and then launch the command :
+```
+	tinypng
+```
+Let the script works !
+
+Enjoy !
---
+++ tinypng.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# source file for api tinypng.
+source .env
+ 
+# Make sure source dir is supplied
+if [ -z "$1" ]
+    then
+    echo "Missing argument. Supply the source directory containing the images to be optimized. Usage: ./tinypng.sh <source_dir>"
+    exit 1
+fi
+ 
+INDIR=$1
+ 
+# Make sure source dir exists
+if [ ! -d "$INDIR" ]; then
+    echo ""$INDIR" directory not found. Supply the source directory containing the images to be optimized. Source directory should be relative to location of this script. Usage: ./tinypng.sh <source_dir>"
+    exit 1
+fi
+ 
+DIRNAME=${INDIR##*/}
+OUTDIRNAME="${DIRNAME}_tiny"
+OUTDIR="`pwd`/$OUTDIRNAME"
+ 
+# Make sure output dir does not already exist with files.
+# If dir exists but empty, it's ok, we proceed.
+if find "$OUTDIRNAME" -mindepth 1 -print -quit | grep -q .; then
+    echo "Output directory ($OUTDIRNAME) already exists. Exiting without optimizing images."
+    exit 1
+fi
+ 
+# Create output dir if it does not already exist
+if [ ! -d "$OUTDIRNAME" ]; then
+    echo "Creating output dir "$OUTDIRNAME"..."
+    mkdir $OUTDIRNAME   
+fi
+ 
+# Begin optimizing images
+echo "Optimizing images..."
+cd $INDIR
+shopt -s nullglob
+for file in *.png *.PNG *.jpg *.JPG *.jpeg *.JPEG
+do
+    Cfile=`curl --dump-header /dev/stdout --silent --user api:$TINYAPIKEY --data-binary @"${file}" https://api.tinify.com/shrink | grep location | awk '{print $2 }'`
+    Cfile=${Cfile// }
+    Cfile=`echo -n "$Cfile"| sed s/.$//`
+    curl --silent $Cfile -o "${OUTDIR}/${file}"
+done
+echo "Optimization done"
+ 
+# Check before replace
+INDIR_FILE_COUNT="$(ls | wc -l)"
+cd $OUTDIR
+OUTDIR_FILE_COUNT="$(ls | wc -l)"
+
+if [ "$INDIR_FILE_COUNT" = "$OUTDIR_FILE_COUNT" ]; then
+    echo "Replacing $INDIR folder with compressed files"
+    d=$(date +%Y-%m-%d_%H%M)
+    cd $INDIR/..
+    tar czvf /tmp/$DIRNAME-$d.tar.gz $DIRNAME
+    rm -rf $INDIR
+    mv $OUTDIR $INDIR
+else
+    echo "Problem when retrieving files from Tinypng"
+    echo "Files form $INDIR : $INDIR_FILE_COUNT"
+    echo "Files retrieved from Tinypng : $OUTDIR_FILE_COUNT"
+fi
